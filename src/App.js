6143bea4a184fbfea48174e689f0ba4c15ef07d2
Nicole Benedict Lim <lim.nicolebenedict3@gmail.com>
2021-09-06 13:57:55 +0800

    added main element

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import './App.css';
 
 import Header from './components/header/Header';
 
 function App() {
   return (
-    <Header/>
+    <React.Fragment>
+      <Header/>
+      <main>
+        
+      </main>
+    </React.Fragment>
   );
 }
 

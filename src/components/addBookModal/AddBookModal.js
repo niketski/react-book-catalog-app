32cf5b3b68e51d8194c8e389022b37101f555575
Nicole Benedict Lim <lim.nicolebenedict3@gmail.com>
2021-09-12 18:56:47 +0800

    added input validation message

--- src/components/addBookModal/AddBookModal.js
+++ src/components/addBookModal/AddBookModal.js
@@ -9,26 +9,32 @@ function AddBookModal(props) {
                     <div className={`inputField ${styles.inputField}`}>
                         <label htmlFor="book-title">ISBN</label>
                         <input type="text" id="book-ISBN" placeholder="Title"/>
+                        <div className="vaildationMessage">invalid.</div>
                     </div>
                     <div className={`inputField ${styles.inputField}`}>
                         <label htmlFor="book-isbn">ISBN</label>
                         <input type="text" id="book-isbn" placeholder="ISBN"/>
+                        <div className="vaildationMessage">invalid.</div>
                     </div>
                     <div className={`inputField ${styles.inputField}`}>
                         <label htmlFor="book-author">Author</label>
                         <input type="text" id="book-author" placeholder="Author"/>
+                        <div className="vaildationMessage">invalid.</div>
                     </div>
                     <div className={`inputField ${styles.inputField}`}>
                         <label htmlFor="book-publisher">Publisher</label>
                         <input type="text" id="book-publisher" placeholder="Publisher"/>
+                        <div className="vaildationMessage">invalid.</div>
                     </div>
                     <div className={`inputField ${styles.inputField}`}>
                         <label htmlFor="book-year-published">Year Published</label>
                         <input type="text" id="book-year-published" placeholder="Year Published"/>
+                        <div className="vaildationMessage">invalid.</div>
                     </div>
                     <div className={`inputField ${styles.inputField}`}>
                         <label htmlFor="book-category">Category</label>
                         <input type="text" id="book-category" placeholder="Category"/>
+                        <div className="vaildationMessage">invalid.</div>
                     </div>
                     <button type="submit" className={`btnPrimary ${styles.btnPrimary}`}>Submit</button>
                 </form>
